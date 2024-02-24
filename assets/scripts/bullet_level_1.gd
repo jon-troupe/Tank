31f2b305d80f0838fdd0027cc149acd4272a47b2
Jonathan Troupe <sanetroupe@gmail.com>
2024-02-24 18:29:51 -0500

    Improved basic bullet, good looking animation now

--- assets/scripts/bullet_level_1.gd
+++ assets/scripts/bullet_level_1.gd
@@ -6,6 +6,7 @@ var target = position
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	target = get_global_mouse_position()
+	$AnimatedSprite2D.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
