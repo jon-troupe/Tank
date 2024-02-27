9866dd5f72f8563bc24fc4ad7c56252b53324270
Jonathan Troupe <sanetroupe@gmail.com>
2024-02-26 23:59:48 -0500

    Restructured file architecture and cleaned up unused files

--- assets/scripts/Gun.gd
+++ assets/scripts/Gun.gd
@@ -4,6 +4,5 @@ extends Sprite2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var mouse_position = get_global_mouse_position()
-	if mouse_position[1] < 452:
+	if mouse_position[1] < 425:
 		look_at(mouse_position)
-	
